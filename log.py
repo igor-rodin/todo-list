58f312e91be89e318d575f948b830bc564ab94c1
Andrey <dron881@mail.ru>
2022-06-13 19:16:52 +0300

    Работа с log

--- log.py
+++ log.py
@@ -4,7 +4,7 @@ import time
 import settings
 
 
-def add2log(text):  # пишем все в лог - файл
+def add2log(text):  # записываем всё в файл-лог
     with open(settings.LOG_FILE, 'a', encoding='UTF-8') as f:
         f.write(
             f'{time.strftime("%Y.%m.%d %H:%M:%S",time.gmtime(time.time()))},{text}\n')
